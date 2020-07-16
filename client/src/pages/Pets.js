3b69002e619fde7769b83e42f4129604c4894613
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-07-16 13:45:30 -0400

    finished graphql course

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,46 +5,76 @@ import PetsList from '../components/PetsList';
 import NewPetModal from '../components/NewPetModal';
 import Loader from '../components/Loader';
 
+const PETS_FIELDS = gql`
+	fragment PetsFields on Pet {
+		id
+		name
+		type
+		img
+		vaccinated @client
+		owner {
+			id
+			age @client
+		}
+	}
+`;
+
 const ALL_PETS = gql`
 	query AllPets {
 		pets {
-			id
-			name
-			type
-			img
+      ...PetsFields
 		}
-	}
+  }
+  ${PETS_FIELDS}
 `;
 
 const NEW_PET = gql`
 	mutation CreateAPet($newPet: NewPetInput!) {
 		addPet(input: $newPet) {
-			id
-			name
-			type
-			img
+    ...PetsFields
 		}
-	}
+  }
+  ${PETS_FIELDS}
 `;
 export default function Pets() {
 	const [ modal, setModal ] = useState(false);
 	const { data, loading, error } = useQuery(ALL_PETS);
-	const [ createPet, newPet ] = useMutation(NEW_PET);
+	const [ createPet, newPet ] = useMutation(NEW_PET, {
+		update(cache, { data: { addPet } }) {
+			const { pets } = cache.readQuery({ query: ALL_PETS });
+			cache.writeQuery({
+				query : ALL_PETS,
+				data  : { pets: pets.concat([ addPet ]) },
+			});
+		},
+	});
 
 	const onSubmit = (input) => {
 		setModal(false);
 		createPet({
-			variables : { newPet: input },
+			variables          : { newPet: input },
+			optimisticResponse : {
+				__typename : 'Mutation',
+				addPet     : {
+					__typename : 'Pet',
+					id         : Math.floor(Math.random() * 10000) + '',
+					name       : input.name,
+					type       : input.type,
+					img        : 'https://via.placeholder.com/300',
+				},
+			},
 		});
 	};
 
-	if (loading || newPet.loading) {
+	if (loading) {
 		return <Loader />;
 	}
 
 	if (error || newPet.error) {
 		return <p>error!</p>;
 	}
+
+	console.log(data.pets[0]);
 
 	if (modal) {
 		return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
